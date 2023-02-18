70d5029434af7bc63758c2a7e68a820c7da80afa
Artur Troian <troian.ap@gmail.com>
2023-02-18 15:37:27 -0500

    fix: unify test targets by using variables (#7)
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- make/test.mk
+++ make/test.mk
@@ -1,36 +1,31 @@
 COVER_PACKAGES = $(shell go list ./... | grep -v mock | paste -sd, -)
 
-###############################################################################
-###                           Misc tests                                    ###
-###############################################################################
-
-# on MacOS disable deprecation warnings security framework
-ifeq ($(DETECTED_OS), Darwin)
-	export CGO_CFLAGS=-Wno-deprecated-declarations
-endif
-
-export CGO_CFLAGS_ALLOW
-
 .PHONY: shellcheck
 shellcheck:
 	docker run --rm \
-	--volume ${PWD}:/shellcheck \
+	--volume $(PWD):/shellcheck \
 	--entrypoint sh \
 	koalaman/shellcheck-alpine:stable \
 	-x /shellcheck/script/shellcheck.sh
 
+TEST_TIMEOUT  ?= 300
+TEST_RACE     ?= 0
+TEST_NOCACHE  ?= 0
+
+test_flags := -timeout $(TEST_TIMEOUT)s
+
+ifeq ($(TEST_NOCACHE), 1)
+test_flags += -count=1
+endif
+
+ifeq ($(TEST_RACE), 1)
+test_flags += -race
+endif
+
 .PHONY: test
 test: CGO_CFLAGS=$(CGO_CFLAGS)
 test:
-	$(GO) test -timeout 300s ./...
-
-.PHONY: test-nocache
-test-nocache:
-	$(GO) test -count=1 ./...
-
-.PHONY: test-full
-test-full:
-	$(GO) test -race ./...
+	$(GO) test $(test_flags) ./...
 
 .PHONY: test-coverage
 test-coverage:
