a66408bbd46568dd8abe114fe81ea93978284df5
Artur Troian <troian.ap@gmail.com>
2023-03-14 08:37:50 -0400

    feat: use cometbft fork of tendermint (#29)
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- tools.go
+++ tools.go
@@ -8,6 +8,8 @@ import (
 	_ "github.com/grpc-ecosystem/grpc-gateway/runtime"
 	_ "google.golang.org/grpc"
 
+	_ "github.com/btcsuite/btcd/chaincfg/chainhash"
+
 	// TODO https://github.com/akash-network/support/issues/77
 	_ "github.com/regen-network/cosmos-proto/protoc-gen-gocosmos"
 )
