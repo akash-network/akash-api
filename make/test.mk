c1e5ed90cdf3c0bce3213250bc170229b5b6aa59
Artur Troian <troian.ap@gmail.com>
2023-06-07 11:28:00 -0400

    test: use variables to allow filtering packages in which to run tests
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- make/test.mk
+++ make/test.mk
@@ -1,3 +1,5 @@
+TEST_MODULE   ?= ./...
+
 COVER_PACKAGES = $(shell go list ./... | grep -v mock | paste -sd, -)
 
 .PHONY: shellcheck
@@ -11,6 +13,7 @@ shellcheck:
 TEST_TIMEOUT  ?= 300
 TEST_RACE     ?= 0
 TEST_NOCACHE  ?= 0
+TEST_VERBOSE  ?= 0
 
 test_flags := -timeout $(TEST_TIMEOUT)s
 
@@ -22,18 +25,22 @@ ifeq ($(TEST_RACE), 1)
 test_flags += -race
 endif
 
+ifeq ($(TEST_VERBOSE), 1)
+test_flags += -v
+endif
+
 .PHONY: test
 test: CGO_CFLAGS=$(CGO_CFLAGS)
 test:
-	$(GO) test $(test_flags) ./...
+	$(GO) test $(test_flags) $(TEST_MODULE)
 
 .PHONY: test-coverage
 test-coverage:
 	$(GO) test -coverprofile=coverage.txt \
 		-covermode=count \
 		-coverpkg="$(COVER_PACKAGES)" \
-		./...
+		$(TEST_MODULE)
 
 .PHONY: test-vet
 test-vet:
-	$(GO) vet ./...
+	$(GO) vet $(TEST_MODULE)
