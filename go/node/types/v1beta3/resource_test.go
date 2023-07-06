18f901b26581e7008a0c67dbe296d4c1d21cd638
Artur Troian <troian.ap@gmail.com>
2023-07-06 14:45:41 -0400

    fix: preallocate slice capacity instead of length
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- go/node/types/v1beta3/resource_test.go
+++ go/node/types/v1beta3/resource_test.go
@@ -1,28 +1,38 @@
 package v1beta3
 
-// func TestResourceUnitsSubIsIdempotent(t *testing.T) {
-// 	ru := ResourceUnits{
-// 		CPU:    &CPU{Units: NewResourceValue(1000)},
-// 		Memory: &Memory{Quantity: NewResourceValue(10 * unit.Gi)},
-// 		Storage: Volumes{
-// 			Storage{Quantity: NewResourceValue(10 * unit.Gi)},
-// 		},
-// 	}
-// 	cpuAsString := ru.CPU.String()
-// 	newRu, err := ru.Sub(
-// 		ResourceUnits{
-// 			CPU:    &CPU{Units: NewResourceValue(1)},
-// 			Memory: &Memory{Quantity: NewResourceValue(0 * unit.Gi)},
-// 			Storage: Volumes{
-// 				Storage{Quantity: NewResourceValue(0 * unit.Gi)},
-// 			},
-// 		},
-// 	)
-// 	require.NoError(t, err)
-// 	require.NotNil(t, newRu)
-//
-// 	cpuAsStringAfter := ru.CPU.String()
-// 	require.Equal(t, cpuAsString, cpuAsStringAfter)
-//
-// 	require.Equal(t, newRu.CPU.GetUnits().Value(), uint64(999))
-// }
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestVolumes_Dup(t *testing.T) {
+	volumes := Volumes{
+		Storage{
+			Name:       "default",
+			Quantity:   NewResourceValue(100),
+			Attributes: Attributes{},
+		},
+	}
+
+	dVolumes := volumes.Dup()
+
+	require.Equal(t, volumes, dVolumes)
+}
+
+func TestVolumes_Equal(t *testing.T) {
+	volumes := Volumes{
+		Storage{
+			Name:       "default",
+			Quantity:   NewResourceValue(100),
+			Attributes: Attributes{},
+		},
+	}
+
+	dVolumes := volumes.Dup()
+
+	require.True(t, volumes.Equal(dVolumes))
+
+	dVolumes[0].Name = "class2"
+	require.False(t, volumes.Equal(dVolumes))
+}
