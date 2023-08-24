097e685623ae8b51d6166fd7c0f40b9b6618c3f2
Artur Troian <troian.ap@gmail.com>
2023-08-24 15:23:55 -0400

    refactor(v1beta): rename leaseId migration func
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- go/node/market/v1beta3/migrate/v1beta2.go
+++ go/node/market/v1beta3/migrate/v1beta2.go
@@ -5,7 +5,7 @@ import (
 	"github.com/akash-network/akash-api/go/node/market/v1beta3"
 )
 
-func LeaseIDToV1beta3(from v1beta2.LeaseID) v1beta3.LeaseID {
+func LeaseIDFromV1beta2(from v1beta2.LeaseID) v1beta3.LeaseID {
 	return v1beta3.LeaseID{
 		Owner:    from.Owner,
 		DSeq:     from.DSeq,
