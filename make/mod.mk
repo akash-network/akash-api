5ce35aa3bedb4d53a8c3f4474579df2987cb8a9e
chainzero <scarruthers@Scotts-MacBook-Pro.local>
2023-10-26 12:46:27 -0400

    feat: inventory api
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- make/mod.mk
+++ make/mod.mk
@@ -30,9 +30,11 @@ modsensure: deps-tidy deps-vendor
 modvendor: export VENDOR_BUF:=$(VENDOR_BUF)
 modvendor: $(MODVENDOR) modsensure
 	@echo "vendoring non-go files..."
-	$(MODVENDOR) -copy="**/*.proto" -include=github.com/cosmos/cosmos-sdk/proto,github.com/cosmos/cosmos-sdk/third_party/proto,github.com/cosmos/ibc-go/v3/proto
+	$(MODVENDOR) -copy="**/*.proto" -include=github.com/cosmos/cosmos-sdk/proto,github.com/cosmos/cosmos-sdk/third_party/proto
 	$(MODVENDOR) -copy="**/Makefile" -include=github.com/cosmos/gogoproto
 	$(MODVENDOR) -copy="**/*.proto" -include=github.com/cosmos/cosmos-proto/proto
 	$(MODVENDOR) -copy="**/swagger.yaml" -include=github.com/cosmos/cosmos-proto/client/docs/swagger-ui
+	$(MODVENDOR) -copy="**/*.proto" -include=k8s.io/apimachinery
+	@echo "$${VENDOR_BUF}" > vendor/k8s.io/apimachinery/pkg/api/resource/buf.yaml
 	@echo "$${VENDOR_BUF}" > vendor/github.com/cosmos/cosmos-sdk/proto/buf.yaml
 	@echo "$${VENDOR_BUF}" > vendor/github.com/cosmos/cosmos-sdk/third_party/proto/buf.yaml
