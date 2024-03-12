7de4efcbb86350f9902e0870aa334cdb0bd054a4
Artur Troian <troian.ap@gmail.com>
2024-03-12 15:50:59 -0400

    feat(go/client): integrate cosmos sdk query clients into api (#135)
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- go/node/client/v1beta2/query.go
+++ go/node/client/v1beta2/query.go
@@ -6,6 +6,18 @@ import (
 	"google.golang.org/grpc"
 
 	sdkclient "github.com/cosmos/cosmos-sdk/client"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	disttypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	evdtypes "github.com/cosmos/cosmos-sdk/x/evidence/types"
+	feegranttypes "github.com/cosmos/cosmos-sdk/x/feegrant"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types/proposal"
+	slashtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
+	staketypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
 	atypes "github.com/akash-network/akash-api/go/node/audit/v1beta3"
 	ctypes "github.com/akash-network/akash-api/go/node/cert/v1beta3"
@@ -14,12 +26,28 @@ import (
 	ptypes "github.com/akash-network/akash-api/go/node/provider/v1beta3"
 )
 
+type sdkQueryClient struct {
+	auth     authtypes.QueryClient
+	authz    authz.QueryClient
+	bank     banktypes.QueryClient
+	distr    disttypes.QueryClient
+	evidence evdtypes.QueryClient
+	feegrant feegranttypes.QueryClient
+	gov      govtypes.QueryClient
+	mint     minttypes.QueryClient
+	params   paramtypes.QueryClient
+	slashing slashtypes.QueryClient
+	staking  staketypes.QueryClient
+	upgrade  upgradetypes.QueryClient
+}
+
 type queryClient struct {
 	dclient dtypes.QueryClient
 	mclient mtypes.QueryClient
 	pclient ptypes.QueryClient
 	aclient atypes.QueryClient
 	cclient ctypes.QueryClient
+	sdk     sdkQueryClient
 	cctx    sdkclient.Context
 }
 
@@ -35,7 +63,21 @@ func newQueryClient(cctx sdkclient.Context) *queryClient {
 		pclient: ptypes.NewQueryClient(cctx),
 		aclient: atypes.NewQueryClient(cctx),
 		cclient: ctypes.NewQueryClient(cctx),
-		cctx:    cctx,
+		sdk: sdkQueryClient{
+			auth:     authtypes.NewQueryClient(cctx),
+			authz:    authz.NewQueryClient(cctx),
+			bank:     banktypes.NewQueryClient(cctx),
+			distr:    disttypes.NewQueryClient(cctx),
+			evidence: evdtypes.NewQueryClient(cctx),
+			feegrant: feegranttypes.NewQueryClient(cctx),
+			gov:      govtypes.NewQueryClient(cctx),
+			mint:     minttypes.NewQueryClient(cctx),
+			params:   paramtypes.NewQueryClient(cctx),
+			slashing: slashtypes.NewQueryClient(cctx),
+			staking:  staketypes.NewQueryClient(cctx),
+			upgrade:  upgradetypes.NewQueryClient(cctx),
+		},
+		cctx: cctx,
 	}
 }
 
@@ -159,31 +201,50 @@ func (c *queryClient) Certificates(ctx context.Context, in *ctypes.QueryCertific
 	return c.cclient.Certificates(ctx, in, opts...)
 }
 
-// func (c *queryClient) Accounts(ctx context.Context, in *authtypes.QueryAccountsRequest, opts ...grpc.CallOption) (*authtypes.QueryAccountsResponse, error) {
-// 	if c.cclient == nil {
-// 		return &authtypes.QueryAccountsResponse{}, ErrClientNotFound
-// 	}
-//
-// 	return c.sdk.auth.Accounts(ctx, in, opts...)
-// }
-//
-// func (c *queryClient) Account(ctx context.Context, in *authtypes.QueryAccountRequest, opts ...grpc.CallOption) (*authtypes.QueryAccountResponse, error) {
-// 	if c.cclient == nil {
-// 		return &authtypes.QueryAccountResponse{}, ErrClientNotFound
-// 	}
-//
-// 	return c.sdk.auth.Account(ctx, in, opts...)
-// }
-//
-// func (c *queryClient) Params(ctx context.Context, in *authtypes.QueryParamsRequest, opts ...grpc.CallOption) (*authtypes.QueryParamsResponse, error) {
-// 	if c.cclient == nil {
-// 		return &authtypes.QueryParamsResponse{}, ErrClientNotFound
-// 	}
-//
-// 	return c.sdk.auth.Accounts(ctx, in, opts...)
-// }
-//
-// func (c *queryClient) ModuleAccountByName(ctx context.Context, in *authtypes.QueryModuleAccountByNameRequest, opts ...grpc.CallOption) (*authtypes.QueryModuleAccountByNameResponse, error) {
-// 	// TODO implement me
-// 	panic("implement me")
-// }
+func (c *queryClient) Auth() authtypes.QueryClient {
+	return c.sdk.auth
+}
+
+func (c *queryClient) Authz() authz.QueryClient {
+	return c.sdk.authz
+}
+
+func (c *queryClient) Bank() banktypes.QueryClient {
+	return c.sdk.bank
+}
+
+func (c *queryClient) Distribution() disttypes.QueryClient {
+	return c.sdk.distr
+}
+
+func (c *queryClient) Evidence() evdtypes.QueryClient {
+	return c.sdk.evidence
+}
+
+func (c *queryClient) Feegrant() feegranttypes.QueryClient {
+	return c.sdk.feegrant
+}
+
+func (c *queryClient) Gov() govtypes.QueryClient {
+	return c.sdk.gov
+}
+
+func (c *queryClient) Mint() minttypes.QueryClient {
+	return c.sdk.mint
+}
+
+func (c *queryClient) Params() paramtypes.QueryClient {
+	return c.sdk.params
+}
+
+func (c *queryClient) Slashing() slashtypes.QueryClient {
+	return c.sdk.slashing
+}
+
+func (c *queryClient) Staking() staketypes.QueryClient {
+	return c.sdk.staking
+}
+
+func (c *queryClient) Upgrade() upgradetypes.QueryClient {
+	return c.sdk.upgrade
+}
