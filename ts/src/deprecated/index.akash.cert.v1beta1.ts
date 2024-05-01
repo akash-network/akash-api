f231845a4ebb48f47d554f400999bbf5bcfb01d8
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-05-01 14:16:45 +0200

    feat(ts): adds several deprecated modules (#184)
    Needed to ensure cloudmos functionality which still relies on the relevant code.

---
+++ ts/src/deprecated/index.akash.cert.v1beta1.ts
@@ -0,0 +1,3 @@
+/* eslint-disable */
+
+export * from './akash/cert/v1beta1/cert';
---
+++ ts/src/deprecated/index.akash.market.v1beta1.ts
@@ -0,0 +1,4 @@
+/* eslint-disable */
+
+export * from './akash/market/v1beta1/bid';
+export * from './akash/market/v1beta1/lease';
