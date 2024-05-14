dd6f4d40f3517e5a5376e5dd0a0ecdcd14d4656a
Iaroslav Gryshaiev <ygrishajev@gmail.com>
2024-05-14 18:53:16 +0200

    feat(ts): add required market exports (#175)
    refs akash-network/support#184

--- ts/src/patch/index.akash.deployment.v1beta3.ts
+++ ts/src/patch/index.akash.deployment.v1beta3.ts
@@ -2,3 +2,4 @@ export * from '../generated/index.akash.deployment.v1beta3';
 export * from '../generated/akash/deployment/v1beta3/groupmsg';
 export * from '../generated/akash/deployment/v1beta3/deploymentmsg';
 export * from '../generated/akash/deployment/v1beta3/authz';
+export * from '../generated/akash/deployment/v1beta3/query';
