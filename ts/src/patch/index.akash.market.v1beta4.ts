e2be77b1807b1c00278b4f149ce5a159e7242b08
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-05-14 11:04:33 +0200

    feat(ts): add required exports
    refs akash-network/support#184

---
+++ ts/src/patch/index.akash.market.v1beta4.ts
@@ -0,0 +1,5 @@
+/* eslint-disable */
+
+export * from './../generated/index.akash.market.v1beta4';
+export * from './../generated/akash/market/v1beta4/query';
+export * from './../generated/akash/market/v1beta4/service';
