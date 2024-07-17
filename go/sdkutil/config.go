936af5af9e182d25a5c6fd67fc1d5918605d29ef
Artur Troian <troian.ap@gmail.com>
2024-07-17 08:05:44 -0500

    fix: disable config seal
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- go/sdkutil/config.go
+++ go/sdkutil/config.go
@@ -1,16 +1,16 @@
 package sdkutil
 
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-)
+// import (
+// 	sdk "github.com/cosmos/cosmos-sdk/types"
+// )
 
 // init atm SDK configs is instantiated with const values, so it sealed within init below
 // it helps for all tests as well as packages relying on this api to always have the same config
 // as soon as sdkutil is imported
-func init() {
-	config := sdk.GetConfig()
-	config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
-	config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
-	config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
-	config.Seal()
-}
+// func init() {
+// 	config := sdk.GetConfig()
+// 	config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
+// 	config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
+// 	config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
+// 	config.Seal()
+// }
