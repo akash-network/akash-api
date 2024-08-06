f3226d4ca0458b9fca999b042a5703b8a3a5c12b
Artur Troian <troian.ap@gmail.com>
2024-08-06 08:47:18 -0500

    feat: move migrations into dedicated module
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- go/node/migrate/types.go
+++ go/node/migrate/types.go
@@ -2,10 +2,39 @@ package migrate
 
 import (
 	"github.com/akash-network/akash-api/go/node/types/v1beta3"
-
+	v1 "pkg.akt.dev/go/node/types/attributes/v1"
 	attr "pkg.akt.dev/go/node/types/attributes/v1/migrate"
 	"pkg.akt.dev/go/node/types/resources/v1beta4"
 )
+
+func AttributesFromV1Beta3(from v1beta3.Attributes) v1.Attributes {
+	res := make(v1.Attributes, 0, len(from))
+
+	for _, attr := range from {
+		res = append(res, v1.Attribute{
+			Key:   attr.Key,
+			Value: attr.Value,
+		})
+	}
+
+	return res
+}
+
+func SignedByFromV1Beta3(from v1beta3.SignedBy) v1.SignedBy {
+	return v1.SignedBy{
+		AllOf: from.AllOf,
+		AnyOf: from.AnyOf,
+	}
+}
+
+func PlacementRequirementsFromV1Beta3(from v1beta3.PlacementRequirements) v1.PlacementRequirements {
+	res := v1.PlacementRequirements{
+		SignedBy:   SignedByFromV1Beta3(from.SignedBy),
+		Attributes: AttributesFromV1Beta3(from.Attributes),
+	}
+
+	return res
+}
 
 func ResourceValueFromV1Beta3(from v1beta3.ResourceValue) v1beta4.ResourceValue {
 	return v1beta4.NewResourceValue(from.Value())
