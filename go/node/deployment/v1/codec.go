b4dbcae096beea98745703b9a2c8e7a3b7ee57c7
Artur Troian <troian.ap@gmail.com>
2024-11-14 10:59:26 -0600

    fix(go): implement GetSignBytes for MsgDepositDeployment
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

---
+++ go/node/deployment/v1/codec.go
@@ -0,0 +1,19 @@
+package v1
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+)
+
+var (
+	// ModuleCdc references the global x/deployment module codec. Note, the codec should
+	// ONLY be used in certain instances of tests and for JSON encoding as Amino is
+	// still used for that purpose.
+	//
+	// The actual codec used for serialization should be provided to x/deployment and
+	// defined at the application level.
+	//
+	// Deprecated: ModuleCdc use is deprecated
+	ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
+)
+
