75d3e893381506f984bca01cb82618cfb2f1c46e
Artur Troian <troian@users.noreply.github.com>
2025-05-16 10:15:37 -0500

    chore: use testdata and specs modules
    Signed-off-by: Artur Troian <troian@users.noreply.github.com>

--- go/util/jwt/jwt_test.go
+++ go/util/jwt/jwt_test.go
@@ -1,184 +1,184 @@
 package jwt
 
-// import (
-// 	"bytes"
-// 	"encoding/json"
-// 	"strings"
-// 	"testing"
-// 	"text/template"
-// 	"time"
-//
-// 	sdk "github.com/cosmos/cosmos-sdk/types"
-// 	"github.com/golang-jwt/jwt/v5"
-// 	"github.com/stretchr/testify/require"
-// 	"github.com/xeipuuv/gojsonschema"
-//
-// 	jwttests "github.com/akash-network/akash-api/testdata/jwt"
-// )
-//
-// type JWTTestSuite struct {
-// 	IntegrationTestSuite
-// }
-//
-// type jwtTestCase struct {
-// 	Description string `json:"description"`
-// 	Claims      Claims `json:"claims"`
-// 	TokenString string `json:"tokenString"`
-// 	Expected    struct {
-// 		SignFail               bool   `json:"signFail"`
-// 		VerifyFail             bool   `json:"verifyFail"`
-// 		BypassSchemaValidation bool   `json:"bypassSchemaValidation"`
-// 		Err                    string `json:"error"`
-// 	} `json:"expected"`
-// }
-//
-// type testTemplate struct {
-// 	Issuer   string
-// 	Provider string
-// 	IatCurr  int64
-// 	Iat24h   int64
-// 	Nbf24h   int64
-// 	NbfCurr  int64
-// 	Exp48h   int64
-// }
-//
-// func (s *JWTTestSuite) initClaims(tc jwtTestCase) jwtTestCase {
-// 	var err error
-//
-// 	if tc.Claims.Issuer != "" {
-// 		tc.Claims.iss, err = sdk.AccAddressFromBech32(tc.Claims.Issuer)
-// 		require.NoError(s.T(), err)
-// 	}
-//
-// 	if tc.TokenString != "" {
-// 		return tc
-// 	}
-//
-// 	ehdr := encodeSegment([]byte(`{"alg":"ES256K","typ":"JWT"}`))
-//
-// 	claims, err := json.Marshal(tc.Claims)
-// 	require.NoError(s.T(), err)
-//
-// 	eclaims := encodeSegment(claims)
-// 	data := ehdr + "." + eclaims
-//
-// 	method := jwt.GetSigningMethod("ES256K")
-// 	sig, err := method.Sign(data, Signer{
-// 		Signer: s.kr,
-// 		addr:   s.info.GetAddress(),
-// 	})
-//
-// 	tc.TokenString = data + "." + encodeSegment(sig)
-//
-// 	return tc
-// }
-//
-// func (s *JWTTestSuite) TestSigning() {
-// 	testCases := s.prepareTestCases(s.T())
-//
-// 	for _, tc := range testCases {
-// 		s.Run(tc.Description, func() {
-// 			token := jwt.NewWithClaims(jwt.GetSigningMethod("ES256K"), tc.Claims)
-// 			tokenString, err := token.SignedString(Signer{
-// 				Signer: s.kr,
-// 				addr:   s.info.GetAddress(),
-// 			})
-//
-// 			if tc.Expected.SignFail {
-// 				require.Error(s.T(), err)
-// 				require.Empty(s.T(), tokenString)
-// 			} else {
-// 				require.NoError(s.T(), err)
-// 				require.Equal(s.T(), tc.TokenString, tokenString)
-// 			}
-//
-// 			if !tc.Expected.VerifyFail {
-// 				claims := &Claims{}
-// 				_, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-// 					return s.info.GetPubKey(), nil
-// 				}, jwt.WithValidMethods([]string{"ES256K"}))
-//
-// 				require.Equal(s.T(), &tc.Claims, claims)
-//
-// 				require.NoError(s.T(), err)
-// 			} else {
-// 				claims := &Claims{}
-// 				_, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-// 					if token.Header["alg"] != "ES256K" {
-// 						return nil, jwt.ErrInvalidKeyType
-// 					}
-// 					return s.info.GetPubKey(), nil
-// 				}, jwt.WithValidMethods([]string{"ES256K"}))
-//
-// 				require.ErrorContains(s.T(), err, tc.Expected.Err)
-// 			}
-// 		})
-// 	}
-// }
-//
-// // TestSchema ensure JSON schema is
-// func (s *JWTTestSuite) TestSchema() {
-// 	testCases := s.prepareTestCases(s.T())
-//
-// 	for _, tc := range testCases {
-// 		s.Run(tc.Description, func() {
-// 			parts := strings.Split(tc.TokenString, ".")
-// 			claims := decodeSegment(s.T(), parts[1])
-//
-// 			res, err := schemaLoader.Validate(gojsonschema.NewBytesLoader(claims))
-// 			require.NotNil(s.T(), res)
-// 			require.NoError(s.T(), err)
-// 			if tc.Expected.VerifyFail && !tc.Expected.BypassSchemaValidation {
-// 				require.False(s.T(), res.Valid())
-// 				require.Greater(s.T(), len(res.Errors()), 0)
-// 			} else {
-// 				require.True(s.T(), res.Valid(), res.Errors())
-// 				require.Len(s.T(), res.Errors(), 0)
-// 			}
-// 		})
-// 	}
-// }
-//
-// func (s *JWTTestSuite) prepareTestCases(t *testing.T) []jwtTestCase {
-// 	t.Helper()
-// 	var testCases []jwtTestCase
-//
-// 	data, err := jwttests.GetTestsFile("cases_jwt.json.tmpl")
-// 	if err != nil {
-// 		s.T().Fatalf("could not read test data file: %v", err)
-// 	}
-//
-// 	now := time.Now()
-//
-// 	testTemplate := testTemplate{
-// 		Issuer:   s.info.GetAddress().String(),
-// 		Provider: s.info.GetAddress().String(),
-// 		IatCurr:  jwt.NewNumericDate(now).Unix(),
-// 		NbfCurr:  jwt.NewNumericDate(now).Unix(),
-// 		Iat24h:   jwt.NewNumericDate(now.Add(24 * time.Hour)).Unix(),
-// 		Nbf24h:   jwt.NewNumericDate(now.Add(24 * time.Hour)).Unix(),
-// 		Exp48h:   jwt.NewNumericDate(now.Add(48 * time.Hour)).Unix(),
-// 	}
-//
-// 	tmpl, err := template.New("tests").Parse(string(data))
-// 	if err != nil {
-// 		s.T().Fatalf("could not parse test data template: %v", err)
-// 	}
-//
-// 	parsedTmpl := &bytes.Buffer{}
-// 	err = tmpl.Execute(parsedTmpl, testTemplate)
-// 	if err != nil {
-// 		s.T().Fatalf("could not execute test data template: %v", err)
-// 	}
-//
-// 	err = json.Unmarshal(parsedTmpl.Bytes(), &testCases)
-// 	if err != nil {
-// 		s.T().Fatalf("could not unmarshal test data: %v", err)
-// 	}
-//
-// 	for i := range testCases {
-// 		testCases[i] = s.initClaims(testCases[i])
-// 	}
-//
-// 	return testCases
-// }
+import (
+	"bytes"
+	"encoding/json"
+	"strings"
+	"testing"
+	"text/template"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/require"
+	"github.com/xeipuuv/gojsonschema"
+
+	jwttests "pkg.akt.dev/testdata/jwt"
+)
+
+type JWTTestSuite struct {
+	IntegrationTestSuite
+}
+
+type jwtTestCase struct {
+	Description string `json:"description"`
+	Claims      Claims `json:"claims"`
+	TokenString string `json:"tokenString"`
+	Expected    struct {
+		SignFail               bool   `json:"signFail"`
+		VerifyFail             bool   `json:"verifyFail"`
+		BypassSchemaValidation bool   `json:"bypassSchemaValidation"`
+		Err                    string `json:"error"`
+	} `json:"expected"`
+}
+
+type testTemplate struct {
+	Issuer   string
+	Provider string
+	IatCurr  int64
+	Iat24h   int64
+	Nbf24h   int64
+	NbfCurr  int64
+	Exp48h   int64
+}
+
+func (s *JWTTestSuite) initClaims(tc jwtTestCase) jwtTestCase {
+	var err error
+
+	if tc.Claims.Issuer != "" {
+		tc.Claims.iss, err = sdk.AccAddressFromBech32(tc.Claims.Issuer)
+		require.NoError(s.T(), err)
+	}
+
+	if tc.TokenString != "" {
+		return tc
+	}
+
+	ehdr := encodeSegment([]byte(`{"alg":"ES256K","typ":"JWT"}`))
+
+	claims, err := json.Marshal(tc.Claims)
+	require.NoError(s.T(), err)
+
+	eclaims := encodeSegment(claims)
+	data := ehdr + "." + eclaims
+
+	method := jwt.GetSigningMethod("ES256K")
+	sig, err := method.Sign(data, Signer{
+		Signer: s.kr,
+		addr:   s.info.GetAddress(),
+	})
+
+	tc.TokenString = data + "." + encodeSegment(sig)
+
+	return tc
+}
+
+func (s *JWTTestSuite) TestSigning() {
+	testCases := s.prepareTestCases(s.T())
+
+	for _, tc := range testCases {
+		s.Run(tc.Description, func() {
+			token := jwt.NewWithClaims(jwt.GetSigningMethod("ES256K"), tc.Claims)
+			tokenString, err := token.SignedString(Signer{
+				Signer: s.kr,
+				addr:   s.info.GetAddress(),
+			})
+
+			if tc.Expected.SignFail {
+				require.Error(s.T(), err)
+				require.Empty(s.T(), tokenString)
+			} else {
+				require.NoError(s.T(), err)
+				require.Equal(s.T(), tc.TokenString, tokenString)
+			}
+
+			if !tc.Expected.VerifyFail {
+				claims := &Claims{}
+				_, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+					return s.info.GetPubKey(), nil
+				}, jwt.WithValidMethods([]string{"ES256K"}))
+
+				require.Equal(s.T(), &tc.Claims, claims)
+
+				require.NoError(s.T(), err)
+			} else {
+				claims := &Claims{}
+				_, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+					if token.Header["alg"] != "ES256K" {
+						return nil, jwt.ErrInvalidKeyType
+					}
+					return s.info.GetPubKey(), nil
+				}, jwt.WithValidMethods([]string{"ES256K"}))
+
+				require.ErrorContains(s.T(), err, tc.Expected.Err)
+			}
+		})
+	}
+}
+
+// TestSchema ensure JSON schema is
+func (s *JWTTestSuite) TestSchema() {
+	testCases := s.prepareTestCases(s.T())
+
+	for _, tc := range testCases {
+		s.Run(tc.Description, func() {
+			parts := strings.Split(tc.TokenString, ".")
+			claims := decodeSegment(s.T(), parts[1])
+
+			res, err := schemaLoader.Validate(gojsonschema.NewBytesLoader(claims))
+			require.NotNil(s.T(), res)
+			require.NoError(s.T(), err)
+			if tc.Expected.VerifyFail && !tc.Expected.BypassSchemaValidation {
+				require.False(s.T(), res.Valid())
+				require.Greater(s.T(), len(res.Errors()), 0)
+			} else {
+				require.True(s.T(), res.Valid(), res.Errors())
+				require.Len(s.T(), res.Errors(), 0)
+			}
+		})
+	}
+}
+
+func (s *JWTTestSuite) prepareTestCases(t *testing.T) []jwtTestCase {
+	t.Helper()
+	var testCases []jwtTestCase
+
+	data, err := jwttests.GetTestsFile("cases_jwt.json.tmpl")
+	if err != nil {
+		s.T().Fatalf("could not read test data file: %v", err)
+	}
+
+	now := time.Now()
+
+	testTemplate := testTemplate{
+		Issuer:   s.info.GetAddress().String(),
+		Provider: s.info.GetAddress().String(),
+		IatCurr:  jwt.NewNumericDate(now).Unix(),
+		NbfCurr:  jwt.NewNumericDate(now).Unix(),
+		Iat24h:   jwt.NewNumericDate(now.Add(24 * time.Hour)).Unix(),
+		Nbf24h:   jwt.NewNumericDate(now.Add(24 * time.Hour)).Unix(),
+		Exp48h:   jwt.NewNumericDate(now.Add(48 * time.Hour)).Unix(),
+	}
+
+	tmpl, err := template.New("tests").Parse(string(data))
+	if err != nil {
+		s.T().Fatalf("could not parse test data template: %v", err)
+	}
+
+	parsedTmpl := &bytes.Buffer{}
+	err = tmpl.Execute(parsedTmpl, testTemplate)
+	if err != nil {
+		s.T().Fatalf("could not execute test data template: %v", err)
+	}
+
+	err = json.Unmarshal(parsedTmpl.Bytes(), &testCases)
+	if err != nil {
+		s.T().Fatalf("could not unmarshal test data: %v", err)
+	}
+
+	for i := range testCases {
+		testCases[i] = s.initClaims(testCases[i])
+	}
+
+	return testCases
+}
--- go/util/jwt/suite_test.go
+++ go/util/jwt/suite_test.go
@@ -1,57 +1,57 @@
 package jwt
 
-// import (
-// 	"context"
-// 	"strings"
-// 	"testing"
-//
-// 	"github.com/cosmos/cosmos-sdk/crypto/hd"
-// 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
-// 	sdk "github.com/cosmos/cosmos-sdk/types"
-// 	"github.com/stretchr/testify/require"
-// 	"github.com/stretchr/testify/suite"
-//
-// 	jwttests "github.com/akash-network/akash-api/testdata/jwt"
-// 	_ "pkg.akt.dev/go/sdkutil"
-// )
-//
-// type IntegrationTestSuite struct {
-// 	suite.Suite
-//
-// 	kr   keyring.Keyring
-// 	info keyring.Info
-// }
-//
-// func (s *IntegrationTestSuite) SetupSuite() {
-// 	s.T().Log("setting up integration test suite")
-//
-// 	mnemonic, err := jwttests.GetTestsFile("mnemonic")
-// 	if err != nil {
-// 		s.T().Fatalf("could not read test data file: %v", err)
-// 	}
-//
-// 	s.kr = keyring.NewInMemory()
-//
-// 	cfg, err := sdk.GetSealedConfig(context.Background())
-// 	require.NoError(s.T(), err)
-//
-// 	hdPath := hd.CreateHDPath(cfg.GetCoinType(), 0, 0).String()
-//
-// 	keyringAlgos, _ := s.kr.SupportedAlgorithms()
-// 	algo, err := keyring.NewSigningAlgoFromString(string(hd.Secp256k1Type), keyringAlgos)
-// 	require.NoError(s.T(), err)
-//
-// 	s.info, err = s.kr.NewAccount("test", strings.TrimSuffix(string(mnemonic), "\n"), "", hdPath, algo)
-// 	require.NoError(s.T(), err)
-//
-// 	s.T().Log("setting up integration test suite done")
-// }
-//
-// func (s *IntegrationTestSuite) TearDownSuite() {
-// 	s.T().Log("tearing down integration test suite")
-// }
-//
-// func TestJWTTestSuite(t *testing.T) {
-// 	suite.Run(t, new(ES256kTest))
-// 	suite.Run(t, new(JWTTestSuite))
-// }
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+
+	_ "pkg.akt.dev/go/sdkutil"
+	jwttests "pkg.akt.dev/testdata/jwt"
+)
+
+type IntegrationTestSuite struct {
+	suite.Suite
+
+	kr   keyring.Keyring
+	info keyring.Info
+}
+
+func (s *IntegrationTestSuite) SetupSuite() {
+	s.T().Log("setting up integration test suite")
+
+	mnemonic, err := jwttests.GetTestsFile("mnemonic")
+	if err != nil {
+		s.T().Fatalf("could not read test data file: %v", err)
+	}
+
+	s.kr = keyring.NewInMemory()
+
+	cfg, err := sdk.GetSealedConfig(context.Background())
+	require.NoError(s.T(), err)
+
+	hdPath := hd.CreateHDPath(cfg.GetCoinType(), 0, 0).String()
+
+	keyringAlgos, _ := s.kr.SupportedAlgorithms()
+	algo, err := keyring.NewSigningAlgoFromString(string(hd.Secp256k1Type), keyringAlgos)
+	require.NoError(s.T(), err)
+
+	s.info, err = s.kr.NewAccount("test", strings.TrimSuffix(string(mnemonic), "\n"), "", hdPath, algo)
+	require.NoError(s.T(), err)
+
+	s.T().Log("setting up integration test suite done")
+}
+
+func (s *IntegrationTestSuite) TearDownSuite() {
+	s.T().Log("tearing down integration test suite")
+}
+
+func TestJWTTestSuite(t *testing.T) {
+	suite.Run(t, new(ES256kTest))
+	suite.Run(t, new(JWTTestSuite))
+}
