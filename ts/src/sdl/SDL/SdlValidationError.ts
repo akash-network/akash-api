6e3cf9bd0d71194d8f85710559bf0bb340956691
Serhii Stotskyi <sergiy.stotskiy@gmail.com>
2025-05-28 06:55:20 +0300

    feat: moves SDL from akashjs (#210)

---
+++ ts/src/sdl/SDL/SdlValidationError.ts
@@ -0,0 +1,23 @@
+export class SdlValidationError extends Error {
+  /**
+   * Asserts a condition and throws an `SdlValidationError` if the condition is false.
+   * @param condition - The condition to assert.
+   * @param message - The error message to throw if the assertion fails.
+   * @example
+   * SdlValidationError.assert(someCondition, "Condition failed");
+   */
+  static assert(condition: unknown, message: string): asserts condition {
+    if (!condition) {
+      throw new SdlValidationError(message);
+    }
+  }
+
+  /**
+   * Creates an instance of `SdlValidationError`.
+   * @param message - The error message.
+   */
+  constructor(message: string) {
+    super(message);
+    this.name = "SdlValidationError";
+  }
+}
