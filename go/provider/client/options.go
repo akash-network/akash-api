3c5e9bad4982e2165a03d7eeb85320e53fbf4b01
Joao Luna <joaopedroluna98@gmail.com>
2025-09-26 19:07:15 +0100

    feat: off-chain provider client (#229)

--- go/provider/client/options.go
+++ go/provider/client/options.go
@@ -2,14 +2,19 @@ package rest
 
 import (
 	"crypto/tls"
+	"errors"
+	"fmt"
 
+	aclient "github.com/akash-network/akash-api/go/node/client/v1beta2"
 	ajwt "github.com/akash-network/akash-api/go/util/jwt"
 )
 
 type clientOptions struct {
-	certs  []tls.Certificate
-	signer ajwt.SignerI
-	token  string
+	certs       []tls.Certificate
+	signer      ajwt.SignerI
+	token       string
+	providerURL string
+	qclient     aclient.QueryClient
 }
 
 // ClientOption is a function type that modifies a clientOptions struct and returns an error.
@@ -23,6 +28,8 @@ type clientOptions struct {
 //
 // If an error occurs while applying the option, it will be returned to the caller.
 type ClientOption func(options *clientOptions) error
+
+var ErrMutuallyExclusiveOptions = errors.New("mutually exclusive options")
 
 // WithAuthCerts configures the client with the provided TLS certificates for secure communication.
 func WithAuthCerts(certs []tls.Certificate) ClientOption {
@@ -45,6 +52,35 @@ func WithAuthJWTSigner(val ajwt.SignerI) ClientOption {
 func WithAuthToken(val string) ClientOption {
 	return func(options *clientOptions) error {
 		options.token = val
+		return nil
+	}
+}
+
+// WithProviderURL configures the client to use the specified provider URL directly.
+// This option is mutually exclusive with WithQueryClient.
+func WithProviderURL(providerURL string) ClientOption {
+	return func(options *clientOptions) error {
+		if options.qclient != nil {
+			return fmt.Errorf("%w: WithProviderURL and WithQueryClient are mutually exclusive", ErrMutuallyExclusiveOptions)
+		}
+		options.providerURL = providerURL
+		return nil
+	}
+}
+
+// WithQueryClient configures the client to use the specified query client for provider discovery.
+// This option is mutually exclusive with WithProviderURL.
+func WithQueryClient(qclient aclient.QueryClient) ClientOption {
+	return func(options *clientOptions) error {
+		if options.providerURL != "" {
+			return fmt.Errorf("%w: WithProviderURL and WithQueryClient are mutually exclusive", ErrMutuallyExclusiveOptions)
+		}
+
+		if qclient == nil {
+			return errors.New("query client is nil")
+		}
+
+		options.qclient = qclient
 		return nil
 	}
 }
